44beda2e71b4011e41b0281535fae6e21dcf6d8d
dzemil karisik <karisikdzemil@gmail.com>
2024-06-11 18:42:14 +0200

    second commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AISP-C
+//
+//  Created by dzemil karisik on 11.6.24..
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
