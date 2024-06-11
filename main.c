5e265ced050509436839bebcc76f8698759eeb70
dzemil karisik <karisikdzemil@gmail.com>
2024-06-12 00:09:15 +0200

    print data

--- main.c
+++ main.c
@@ -7,20 +7,35 @@ struct node {
     struct node *link;
 };
 
+void print_data (struct node *head){
+    struct node *ptr = head;
+    if (head == NULL){
+        printf("list is empty");
+    }else{
+        while(ptr != NULL){
+            printf("%d\n", ptr -> data);
+            ptr = ptr -> link;
+        }
+    }
+}
+
 int main(){
     struct node *head = malloc(sizeof(struct node));
     head -> data=45;
     head -> link = NULL;
     
     struct node *current = malloc (sizeof(struct node));
-    head -> link = current;
-    current -> data = 85;
+    current -> data = 98;
     current -> link = NULL;
+    head -> link = current;
     
-    struct node *third = malloc(sizeof(struct node));
-    third -> data = 21;
-    third -> link = NULL;
-    current -> link = third;
+    current = malloc(sizeof(struct node));
+    current -> data = 3;
+    current -> link = NULL;
+    head ->link ->link = current;
+    print_data(head);
+    
     
     return 0;
 }
+
