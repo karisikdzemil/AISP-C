a8e138f882f21877f1c84ee9bb9d487bb7741207
dzemil karisik <karisikdzemil@gmail.com>
2024-06-12 17:12:30 +0200

    add at end

--- main.c
+++ main.c
@@ -19,11 +19,25 @@ void print_data (struct node *head){
     }
 }
 
+void add_at_end (struct node *head, int data){
+    struct node *ptr, *temp;
+    ptr = head;
+    temp = malloc(sizeof(struct node));
+    
+    temp -> data = data;
+    temp -> link = NULL;
+    
+    while(ptr -> link != NULL){
+        ptr = ptr -> link;
+    }
+    ptr -> link = temp;
+}
+
+
 int main(){
     struct node *head = malloc(sizeof(struct node));
     head -> data=45;
     head -> link = NULL;
-    
     struct node *current = malloc (sizeof(struct node));
     current -> data = 98;
     current -> link = NULL;
@@ -33,9 +47,11 @@ int main(){
     current -> data = 3;
     current -> link = NULL;
     head ->link ->link = current;
+    
+    
+    
+    add_at_end(head, 67);
     print_data(head);
-    
-    
     return 0;
 }
 
