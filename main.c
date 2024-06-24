03ed2867a7c17bf393227e396c19ab46b5f863ea
dzemil karisik <karisikdzemil@gmail.com>
2024-06-24 17:50:08 +0200

    challenge

--- main.c
+++ main.c
@@ -1,73 +1,142 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//struct node {
+//    int data;
+//    struct node *link;
+//};
+//
+//void print_data(struct node *head) {
+//    struct node *ptr = head;
+//    if (head == NULL) {
+//        printf("list is empty\n");
+//    } else {
+//        while (ptr != NULL) {
+//            printf("%d\n", ptr->data);
+//            ptr = ptr->link;
+//        }
+//    }
+//}
+//
+//void add_at_end(struct node *head, int data) {
+//    struct node *ptr, *temp;
+//    ptr = head;
+//    temp = malloc(sizeof(struct node));
+//
+//    temp->data = data;
+//    temp->link = NULL;
+//
+//    while (ptr->link != NULL) {
+//        ptr = ptr->link;
+//    }
+//    ptr->link = temp;
+//}
+//
+//// Funkcija add_beg je izmenjena da koristi pokazivač na pokazivač (struct node **head_ref)
+//void add_beg(struct node **head_ref, int data) {
+//    struct node *temp = malloc(sizeof(struct node));
+//    temp->data = data;
+//    temp->link = *head_ref;
+//    *head_ref = temp;
+//}
+//
+//int main() {
+//    struct node *head = malloc(sizeof(struct node));
+//    head->data = 45;
+//    head->link = NULL;
+//
+//    struct node *current = malloc(sizeof(struct node));
+//    current->data = 98;
+//    current->link = NULL;
+//    head->link = current;
+//
+//    current = malloc(sizeof(struct node));
+//    current->data = 3;
+//    current->link = NULL;
+//    head->link->link = current;
+//
+//    // Pozivamo add_beg sa &head da bismo prosledili pokazivač na pokazivač
+//    add_beg(&head, 10);
+//    add_at_end(head, 67);
+//    print_data(head);
+//
+//    // Oslobađanje memorije
+//    struct node *ptr = head;
+//    struct node *temp;
+//    while (ptr != NULL) {
+//        temp = ptr;
+//        ptr = ptr->link;
+//        free(temp);
+//    }
+//
+//    return 0;
+//}
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-struct node {
+typedef struct Node {
     int data;
-    struct node *link;
-};
+    struct Node* next;
+    struct Node* prev;
+} NODE;
 
-void print_data(struct node *head) {
-    struct node *ptr = head;
-    if (head == NULL) {
-        printf("list is empty\n");
-    } else {
-        while (ptr != NULL) {
-            printf("%d\n", ptr->data);
-            ptr = ptr->link;
+NODE* createNode(int data) {
+    NODE* newNode = (NODE*)malloc(sizeof(NODE));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+void append(NODE** head_ref, int new_data) {
+    NODE* newNode = createNode(new_data);
+    NODE* last = *head_ref;
+    
+    if (*head_ref == NULL) {
+        *head_ref = newNode;
+        return;
+    }
+
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    
+    last->next = newNode;
+    newNode->prev = last;
+}
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+void stampajProste(NODE* head) {
+    NODE* temp = head;
+    while (temp != NULL) {
+        if (isPrime(temp->data)) {
+            printf("%d ", temp->data);
         }
+        temp = temp->next;
     }
-}
-
-void add_at_end(struct node *head, int data) {
-    struct node *ptr, *temp;
-    ptr = head;
-    temp = malloc(sizeof(struct node));
-
-    temp->data = data;
-    temp->link = NULL;
-
-    while (ptr->link != NULL) {
-        ptr = ptr->link;
-    }
-    ptr->link = temp;
-}
-
-// Funkcija add_beg je izmenjena da koristi pokazivač na pokazivač (struct node **head_ref)
-void add_beg(struct node **head_ref, int data) {
-    struct node *temp = malloc(sizeof(struct node));
-    temp->data = data;
-    temp->link = *head_ref;
-    *head_ref = temp;
+    printf("\n");
 }
 
 int main() {
-    struct node *head = malloc(sizeof(struct node));
-    head->data = 45;
-    head->link = NULL;
+    NODE* head = NULL;
+    
+    append(&head, 11);
+    append(&head, 20);
+    append(&head, 17);
+    append(&head, 23);
+    append(&head, 15);
 
-    struct node *current = malloc(sizeof(struct node));
-    current->data = 98;
-    current->link = NULL;
-    head->link = current;
-
-    current = malloc(sizeof(struct node));
-    current->data = 3;
-    current->link = NULL;
-    head->link->link = current;
-
-    // Pozivamo add_beg sa &head da bismo prosledili pokazivač na pokazivač
-    add_beg(&head, 10);
-    add_at_end(head, 67);
-    print_data(head);
-
-    // Oslobađanje memorije
-    struct node *ptr = head;
-    struct node *temp;
-    while (ptr != NULL) {
-        temp = ptr;
-        ptr = ptr->link;
-        free(temp);
-    }
+    printf("Prosti brojevi u listi su: ");
+    stampajProste(head);
 
     return 0;
 }
