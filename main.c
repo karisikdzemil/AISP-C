a3e8a4fb61d2f103a926e2ca6179744f22f7d010
dzemil karisik <karisikdzemil@gmail.com>
2024-06-25 20:05:07 +0200

    the beginning

--- main.c
+++ main.c
@@ -72,71 +72,71 @@
 //    return 0;
 //}
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-typedef struct Node {
-    int data;
-    struct Node* next;
-    struct Node* prev;
-} NODE;
-
-NODE* createNode(int data) {
-    NODE* newNode = (NODE*)malloc(sizeof(NODE));
-    newNode->data = data;
-    newNode->next = NULL;
-    newNode->prev = NULL;
-    return newNode;
-}
-
-void append(NODE** head_ref, int new_data) {
-    NODE* newNode = createNode(new_data);
-    NODE* last = *head_ref;
-    
-    if (*head_ref == NULL) {
-        *head_ref = newNode;
-        return;
-    }
-
-    while (last->next != NULL) {
-        last = last->next;
-    }
-    
-    last->next = newNode;
-    newNode->prev = last;
-}
-
-bool isPrime(int n) {
-    if (n <= 1) return false;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) return false;
-    }
-    return true;
-}
-
-void stampajProste(NODE* head) {
-    NODE* temp = head;
-    while (temp != NULL) {
-        if (isPrime(temp->data)) {
-            printf("%d ", temp->data);
-        }
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-int main() {
-    NODE* head = NULL;
-    
-    append(&head, 11);
-    append(&head, 20);
-    append(&head, 17);
-    append(&head, 23);
-    append(&head, 15);
-
-    printf("Prosti brojevi u listi su: ");
-    stampajProste(head);
-
-    return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdbool.h>
+//
+//typedef struct Node {
+//    int data;
+//    struct Node* next;
+//    struct Node* prev;
+//} NODE;
+//
+//NODE* createNode(int data) {
+//    NODE* newNode = (NODE*)malloc(sizeof(NODE));
+//    newNode->data = data;
+//    newNode->next = NULL;
+//    newNode->prev = NULL;
+//    return newNode;
+//}
+//
+//void append(NODE** head_ref, int new_data) {
+//    NODE* newNode = createNode(new_data);
+//    NODE* last = *head_ref;
+//    
+//    if (*head_ref == NULL) {
+//        *head_ref = newNode;
+//        return;
+//    }
+//
+//    while (last->next != NULL) {
+//        last = last->next;
+//    }
+//    
+//    last->next = newNode;
+//    newNode->prev = last;
+//}
+//
+//bool isPrime(int n) {
+//    if (n <= 1) return false;
+//    for (int i = 2; i * i <= n; i++) {
+//        if (n % i == 0) return false;
+//    }
+//    return true;
+//}
+//
+//void stampajProste(NODE* head) {
+//    NODE* temp = head;
+//    while (temp != NULL) {
+//        if (isPrime(temp->data)) {
+//            printf("%d ", temp->data);
+//        }
+//        temp = temp->next;
+//    }
+//    printf("\n");
+//}
+//
+//int main() {
+//    NODE* head = NULL;
+//    
+//    append(&head, 11);
+//    append(&head, 20);
+//    append(&head, 17);
+//    append(&head, 23);
+//    append(&head, 15);
+//
+//    printf("Prosti brojevi u listi su: ");
+//    stampajProste(head);
+//
+//    return 0;
+//}
