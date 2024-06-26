59d1cd4d81ab5f23b394f786c06bfafc4ff68ace
dzemil karisik <karisikdzemil@gmail.com>
2024-06-26 21:39:18 +0200

    create node

--- main.c
+++ main.c
@@ -140,3 +140,22 @@
 //
 //    return 0;
 //}
+
+// ISPOCETKA/////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+int main (void){
+    struct node *head = NULL;
+    head = (struct node *)malloc(sizeof(struct node));
+    
+    head->data = 45;
+    printf("%d\n", head->data);
+    return 0;
+}
