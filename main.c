7a77a9f8e7c56dc233ed07e9432b1bb6b13758a3
dzemil karisik <karisikdzemil@gmail.com>
2024-09-27 20:34:36 +0200

    exercise

--- main.c
+++ main.c
@@ -1,60 +1,84 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//typedef struct node {
+//    struct node *left;
+//    int data;
+//    struct node *right;
+//} node;
+//
+//node *create(int data) {
+//    node *newNode = malloc(sizeof(node));
+//    newNode->left = NULL;
+//    newNode->data = data;
+//    newNode->right = NULL;
+//    return newNode;
+//}
+//
+//node *insert(node *root, int data) {
+//    if (root == NULL) {
+//        root = create(data);
+//        return root;
+//    }
+//    if (data < root->data) {
+//        root->left = insert(root->left, data);
+//    } else if (data > root->data) {
+//        root->right = insert(root->right, data);
+//    }
+//    return root;
+//}
+//
+//void print(node *root) {
+//    if (root == NULL) {
+//        return;
+//    }
+//  
+//    print(root->left);
+//    printf("%d\n", root->data);
+//    print(root->right);
+//}
+//int sumTree (node *root){
+//    if(root == NULL){
+//        return 0;
+//    }
+//    return sumTree(root->left) + root->data + sumTree(root->right);
+//}
+//
+//int main(void) {
+//    node *root = NULL;
+//    root = insert(root, 5);
+//    root = insert(root, 4);
+//    root = insert(root, 6);
+//    root = insert(root, 3);
+//    root = insert(root, 2);
+//    print(root);
+//    printf("Zbir stabla je: %d", sumTree(root));
+//    return 0;
+//}
 #include <stdio.h>
-#include <stdlib.h>
+#define SIZE 13
 
-typedef struct node {
-    struct node *left;
-    int data;
-    struct node *right;
-} node;
+void directHash(int arr[], int n) {
+    int hashTable[SIZE] = {0};
 
-node *create(int data) {
-    node *newNode = malloc(sizeof(node));
-    newNode->left = NULL;
-    newNode->data = data;
-    newNode->right = NULL;
-    return newNode;
+    for (int i = 0; i < n; i++) {
+        int index = arr[i] % SIZE;
+        hashTable[index] = arr[i];
+    }
+
+    printf("Direct Hashing:\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("Index %d: %d\n", i, hashTable[i]);
+    }
 }
 
-node *insert(node *root, int data) {
-    if (root == NULL) {
-        root = create(data);
-        return root;
-    }
-    if (data < root->data) {
-        root->left = insert(root->left, data);
-    } else if (data > root->data) {
-        root->right = insert(root->right, data);
-    }
-    return root;
-}
-
-void print(node *root) {
-    if (root == NULL) {
-        return;
-    }
-  
-    print(root->left);
-    printf("%d\n", root->data);
-    print(root->right);
-}
-int sumTree (node *root){
-    if(root == NULL){
-        return 0;
-    }
-    return sumTree(root->left) + root->data + sumTree(root->right);
-}
-
-int main(void) {
-    node *root = NULL;
-    root = insert(root, 5);
-    root = insert(root, 4);
-    root = insert(root, 6);
-    root = insert(root, 3);
-    root = insert(root, 2);
-    print(root);
-    printf("Zbir stabla je: %d", sumTree(root));
+int main() {
+    int arr[] = {18, 41, 22, 13, 59, 32, 31, 73};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    directHash(arr, n);
     return 0;
 }
+
 
 //#include <stdio.h>
 //#include <stdlib.h>
